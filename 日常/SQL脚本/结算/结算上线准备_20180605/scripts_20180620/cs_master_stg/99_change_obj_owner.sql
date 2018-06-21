7d3a9d9e748bcc439584c1d320461231a9181d22
RayLee <349278769@qq.com>
2018-06-21 17:15:12 +0800

    modify by raylee

---
+++ 日常/SQL脚本/结算/结算上线准备_20180605/scripts_20180620/cs_master_stg/99_change_obj_owner.sql
@@ -0,0 +1,24 @@
+DO language plpgsql $$
+DECLARE
+	owner VARCHAR(20):='cs_master_stg';
+	i RECORD;
+BEGIN
+	FOR I IN (select relname from pg_class a inner join pg_namespace b on a.relnamespace=b.oid where nspname='public' and upper(relname) not like 'PK%' and relname not like 'dblink%' order by 1)
+	LOOP
+		EXECUTE 'ALTER TABLE '||I.relname||' OWNER TO '||owner;
+	END LOOP;
+	FOR I IN (SELECT VIEWNAME FROM PG_VIEWS where SCHEMANAME='public' order by 1)
+	LOOP
+		EXECUTE 'ALTER VIEW '||I.VIEWNAME||' OWNER TO '||owner;
+	END LOOP;
+	FOR I IN (select relname from pg_class where relname like 'seq\_%' order by 1)
+	LOOP
+		EXECUTE 'ALTER SEQUENCE '||I.relname||' owned by none';
+		EXECUTE 'ALTER SEQUENCE '||I.relname||' owner TO '||owner;
+	END LOOP;
+	FOR I IN (SELECT matviewname FROM pg_matviews)
+	LOOP
+		EXECUTE 'alter MATERIALIZED view '||I.matviewname||' owner TO '||OWNER;
+	END LOOP;
+END;
+$$;
